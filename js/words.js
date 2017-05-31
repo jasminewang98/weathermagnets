4f7953bbfbb4c953461a247d26bb200eefc22eef
kpandza <kaylapandza@Kaylas-Laptop.local>
2017-05-31 19:02:55 -0400

    Merged weather.js with words.js Merged weather.js with words.js

--- js/words.js
+++ js/words.js
@@ -1,4 +1,12 @@
 
+//is the JSON data from the OpenWeatherMap API
+var weather;
+//Is the latitude that the user is @
+var latitude; 
+//Is the longitude that the user is at
+var longitude; 
+
+
 var stage = new Kinetic.Stage({
     container: "container",
     width: window.innerWidth,
@@ -47,4 +55,56 @@ for(w in words) {
 }
 
 stage.add(layer);
+
+
+
+function setup() {
+
+	//If the navigator exists on the user's browser, then it will grab their location. 
+	if(navigator.geolocation){
+		//finds and watches the user's location
+		navigator.geolocation.getCurrentPosition(onPositionRecieved, locationNotRecieved, {timeout:0});
+		var watch = navigator.geolocation.watchPosition(onPositionRecieved, locationNotRecieved);
+		
+
+	}
+	//the canvas is then created
+   createCanvas(500,500);
+
+
+}
+
+function onPositionRecieved(position){
+
+	//when the position is recieved, the latitude and longitude are read and then the JSON is loaded
+	latitude = position.coords.latitude; 
+	longitude = position.coords.longitude; 
+
+	var urlPreLat = 'http://api.openweathermap.org/data/2.5/weather?lat='; 
+   var urlPostLat = '&lon=';
+   var urlPostLon = '&APPID=dbbaf2eb6a366eedbd59b8c908bb4cac&units=metric'; 
+
+
+   var jsonCall = urlPreLat + latitude + urlPostLat + longitude + urlPostLon;
+   loadJSON(jsonCall, gotData);
+}
+
+function draw() {
+	
+	
+}
+
+//an error is thrown if the location is not recieved
+function locationNotRecieved(PositionError){
+	console.log(PositionError);
+}
+
+//weather data is saved into the weather var
+function gotData(data){
+
+		weather = data; 
+	
+	
+
+}
 
